a3887ddadbd063810626258059803f30422e47b2
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-08-15 19:02:33 +0100

    Rename fonts to use lowercase letters

--- app/src/main/java/tools/keystroke/mono/ui/theme/Fonts.kt
+++ app/src/main/java/tools/keystroke/mono/ui/theme/Fonts.kt
@@ -6,24 +6,24 @@ import androidx.compose.ui.text.font.FontWeight
 import tools.keystroke.mono.R
 
 val interFamily = FontFamily(
-    Font(R.font.Inter_Thin, FontWeight.Thin),
-    Font(R.font.Inter_ExtraLight, FontWeight.ExtraLight),
-    Font(R.font.Inter_Light, FontWeight.Light),
-    Font(R.font.Inter_Medium, FontWeight.Medium),
-    Font(R.font.Inter_Regular, FontWeight.Normal),
-    Font(R.font.Inter_SemiBold, FontWeight.SemiBold),
-    Font(R.font.Inter_Bold, FontWeight.Bold),
+    Font(R.font.inter_thin, FontWeight.Thin),
+    Font(R.font.inter_extralight, FontWeight.ExtraLight),
+    Font(R.font.inter_light, FontWeight.Light),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_semibold, FontWeight.SemiBold),
+    Font(R.font.inter_bold, FontWeight.Bold),
 )
 
 val VT323 = FontFamily(
-    Font(R.font.VT323_Regular, FontWeight.Normal),
+    Font(R.font.vt323_regular, FontWeight.Normal),
 )
 
 val mozillaTextFamily = FontFamily(
-    Font(R.font.MozillaText_ExtraLight, FontWeight.ExtraLight),
-    Font(R.font.MozillaText_Light, FontWeight.Light),
-    Font(R.font.MozillaText_Regular, FontWeight.Normal),
-    Font(R.font.MozillaText_Medium, FontWeight.Medium),
-    Font(R.font.MozillaText_Bold, FontWeight.Bold),
-    Font(R.font.MozillaText_SemiBold, FontWeight.SemiBold),
+    Font(R.font.mozillatext_extralight, FontWeight.ExtraLight),
+    Font(R.font.mozillatext_light, FontWeight.Light),
+    Font(R.font.mozillatext_regular, FontWeight.Normal),
+    Font(R.font.mozillatext_medium, FontWeight.Medium),
+    Font(R.font.mozillatext_bold, FontWeight.Bold),
+    Font(R.font.mozillatext_semibold, FontWeight.SemiBold),
 )
