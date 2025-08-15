0edbdb4d83e8f5b58d95bec592edde62708e5c20
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-08-15 18:38:37 +0100

    Add fonts definition

--- app/src/main/java/tools/keystroke/mono/ui/theme/Fonts.kt
+++ app/src/main/java/tools/keystroke/mono/ui/theme/Fonts.kt
@@ -1,2 +1,29 @@
 package tools.keystroke.mono.ui.theme
 
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import tools.keystroke.mono.R
+
+val interFamily = FontFamily(
+    Font(R.font.Inter_Thin, FontWeight.Thin),
+    Font(R.font.Inter_ExtraLight, FontWeight.ExtraLight),
+    Font(R.font.Inter_Light, FontWeight.Light),
+    Font(R.font.Inter_Medium, FontWeight.Medium),
+    Font(R.font.Inter_Regular, FontWeight.Normal),
+    Font(R.font.Inter_SemiBold, FontWeight.SemiBold),
+    Font(R.font.Inter_Bold, FontWeight.Bold),
+)
+
+val VT323 = FontFamily(
+    Font(R.font.VT323_Regular, FontWeight.Normal),
+)
+
+val mozillaTextFamily = FontFamily(
+    Font(R.font.MozillaText_ExtraLight, FontWeight.ExtraLight),
+    Font(R.font.MozillaText_Light, FontWeight.Light),
+    Font(R.font.MozillaText_Regular, FontWeight.Normal),
+    Font(R.font.MozillaText_Medium, FontWeight.Medium),
+    Font(R.font.MozillaText_Bold, FontWeight.Bold),
+    Font(R.font.MozillaText_SemiBold, FontWeight.SemiBold),
+)
