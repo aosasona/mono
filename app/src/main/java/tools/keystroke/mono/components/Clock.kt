4fadb52a82bc4caea811017eb3105120594dd9d2
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-08-16 21:19:46 +0100

    Fix font and margin between time and date

--- app/src/main/java/tools/keystroke/mono/components/Clock.kt
+++ app/src/main/java/tools/keystroke/mono/components/Clock.kt
@@ -1,10 +1,14 @@
 package tools.keystroke.mono.components
 
-import android.os.SystemClock
 import android.provider.Settings
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -16,9 +20,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,7 +35,9 @@ import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 import kotlinx.coroutines.delay
-import tools.keystroke.mono.ui.theme.VT323
+import tools.keystroke.mono.ui.theme.mozillaTextFamily
+import kotlin.math.max
+import kotlin.math.min
 
 @Composable
 fun rememberIs24HourFormat(): Boolean {
@@ -54,7 +63,9 @@ fun Clock(
     use24HourFormat: Boolean = rememberIs24HourFormat(),
     showDate: Boolean = true,
     showSeconds: Boolean = false,
-    clockSize: Int = 80,
+
+    clockSize: Int = 120,
+    dateSize: Int = 22,
 ) {
     var now by remember { mutableStateOf(ZonedDateTime.now(zoneId)) }
     LaunchedEffect(zoneId) {
@@ -85,7 +96,8 @@ fun Clock(
         DateTimeFormatter.ofPattern("EE, d MMM yyyy", Locale.getDefault())
     }
 
-    Column(horizontalAlignment = Alignment.Start) {
+
+    Column(horizontalAlignment = Alignment.Start, modifier = modifier) {
         if (showDate) {
             Text(
                 text = currentDate.format(now),
@@ -93,38 +105,49 @@ fun Clock(
                 style =
                     TextStyle(
                         color = MaterialTheme.colorScheme.onBackground,
-                        fontFamily = VT323,
-                        fontSize = (clockSize / 3).sp,
-                        fontWeight = FontWeight.Normal,
+                        fontFamily = mozillaTextFamily,
+                        fontSize = dateSize.sp,
+                        fontWeight = FontWeight.ExtraLight,
                         textAlign = TextAlign.Start,
                     )
             )
         }
 
-        Row(verticalAlignment = Alignment.Top) {
+        Row(
+            verticalAlignment = Alignment.Top,
+            modifier = modifier
+                .height(IntrinsicSize.Min)
+                .offset(y = (-(dateSize * .5f)).dp)
+        ) {
             Text(
                 text = currentTime.format(now),
-                modifier = modifier,
+                maxLines = 1,
+                softWrap = false,
                 style =
                     TextStyle(
                         color = MaterialTheme.colorScheme.onBackground,
-                        fontFamily = VT323,
+                        fontFamily = mozillaTextFamily,
+                        fontWeight = FontWeight.Bold,
                         fontSize = clockSize.sp,
-                        fontWeight = FontWeight.Normal,
                         textAlign = TextAlign.Start,
+                        platformStyle = PlatformTextStyle(includeFontPadding = false),
+                        lineHeightStyle = LineHeightStyle(
+                            alignment = LineHeightStyle.Alignment.Proportional,
+                            trim = LineHeightStyle.Trim.Both
+                        )
                     )
             )
 
             if (!use24HourFormat) {
                 Text(
                     text = DateTimeFormatter.ofPattern("a", Locale.getDefault()).format(now),
-                    modifier = modifier.padding(top = 12.dp, start = 4.dp),
+                    modifier = modifier.padding(top = 16.dp, start = 4.dp),
                     style =
                         TextStyle(
                             color = MaterialTheme.colorScheme.onBackground,
-                            fontFamily = VT323,
-                            fontSize = (clockSize / 2.5).sp,
-                            fontWeight = FontWeight.Normal,
+                            fontFamily = mozillaTextFamily,
+                            fontSize = (clockSize / 3).sp,
+                            fontWeight = FontWeight.Light,
                             textAlign = TextAlign.Start,
                         )
                 )
