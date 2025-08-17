4a03111c2f9fc848563cd404937a2747ebaa7c7e
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-08-17 20:00:25 +0100

    Minor update to entry point

--- app/src/main/java/tools/keystroke/mono/MainActivity.kt
+++ app/src/main/java/tools/keystroke/mono/MainActivity.kt
@@ -14,6 +14,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { MonoTheme { Scaffold(modifier = Modifier.fillMaxSize()) { _ -> HomeView() } } }
+        setContent {
+            MonoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
+                    HomeView(paddingValues)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/tools/keystroke/mono/views/RootView.kt
+++ app/src/main/java/tools/keystroke/mono/views/RootView.kt
@@ -1,22 +1,33 @@
 package tools.keystroke.mono.views
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import tools.keystroke.mono.components.Clock
+import tools.keystroke.mono.components.HomeStatusBar
 import tools.keystroke.mono.ui.theme.MonoTheme
 
 @Composable
-fun HomeView() {
-    Column {
-        // TODO: add status bar here
-
+// TODO: load user settings
+fun HomeView(
+    paddingValues: PaddingValues,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.padding(paddingValues)) {
+        HomeStatusBar()
         Clock()
+
+        Spacer(modifier = Modifier)
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun HomePreview() {
-    MonoTheme { HomeView() }
+    MonoTheme { HomeView(paddingValues = PaddingValues(16.dp)) }
 }
