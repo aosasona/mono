a0469ac89065208fa56258f4d2488c469100a419
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-08-18 20:52:44 +0100

    Make app fullscreen

--- app/src/main/java/tools/keystroke/mono/MainActivity.kt
+++ app/src/main/java/tools/keystroke/mono/MainActivity.kt
@@ -1,12 +1,15 @@
 package tools.keystroke.mono
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import tools.keystroke.mono.ui.theme.MonoTheme
 import tools.keystroke.mono.views.HomeView
 
@@ -14,12 +17,27 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         setContent {
             MonoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
                     HomeView(paddingValues)
                 }
             }
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            WindowInsetsControllerCompat(window, window.decorView)
+                .hide(WindowInsetsCompat.Type.systemBars())
         }
     }
 }
