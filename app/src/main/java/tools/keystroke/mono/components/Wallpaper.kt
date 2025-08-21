53ed7e0624df9af4d0914d528a5b105a8c237fde
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-08-21 23:37:14 +0100

    WIP: wallpaper component

--- app/src/main/java/tools/keystroke/mono/components/Wallpaper.kt
+++ app/src/main/java/tools/keystroke/mono/components/Wallpaper.kt
@@ -1,2 +1,11 @@
 package tools.keystroke.mono.components
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Wallpaper(
+    modifier: Modifier = Modifier
+) {
+    TODO("Implement wallpaper background component")
+}
